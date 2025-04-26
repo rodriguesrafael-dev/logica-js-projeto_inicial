b065be0fa841b346319e9e27280f9550d8f265cb
rafaelrodrigues <rodriguesrafael.dev@gmail.com>
2025-04-26 19:23:36 -0300

    feat: criado prompt para o usuario escolher um numero entre 1 e 30

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
-alert('Boas vindas ao jogo do numero secreto!')
-let numeroSecreto = 29
+alert('Boas vindas ao jogo do numero secreto!');
+let numeroSecreto = 29;
+prompt('Escolha um numero entre 1 e 30');
