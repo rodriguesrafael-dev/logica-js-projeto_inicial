2e8126a1a11d694678c6ef1866c630d8329c6185
rafaelrodrigues <rodriguesrafael.dev@gmail.com>
2025-04-26 19:35:44 -0300

    feat: criacao da condicional que verifica se o usuario acertou ou nao o numero secreto

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 alert('Boas vindas ao jogo do numero secreto!');
 let numeroSecreto = 5;
 let chute = prompt('Escolha um numero entre 1 e 10');
+
+if (chute == numeroSecreto) {
+    console.log('Isso ai! Voce descbriu o numero secreto (5)');
+}
